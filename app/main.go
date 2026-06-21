570fce6a47333dacc42b42286c463aa53ee60b84
myat-kyaw-thu <149857208+myat-kyaw-thu@users.noreply.github.com>
2026-06-21 10:23:59 +0630

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,6 +7,12 @@ import (
 	"strings"
 )
 
+var builtins = map[string]bool{
+	"echo": true,
+	"exit": true,
+	"type": true,
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
@@ -25,6 +31,13 @@ func main() {
 		} else if strings.HasPrefix(input, "echo ") {
 			message := strings.TrimPrefix(input, "echo ")
 			fmt.Println(message)
+		} else if strings.HasPrefix(input, "type ") {
+			arg := strings.TrimPrefix(input, "type ")
+			if builtins[arg] {
+				fmt.Printf("%s is a shell builtin\n", arg)
+			} else {
+				fmt.Printf("%s: not found\n", arg)
+			}
 		} else {
 			fmt.Printf("%s: command not found\n", input)
 		}
